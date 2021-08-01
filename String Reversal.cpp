af664968deeb0bc7c2c8362014b8210e9d98abca
Naveen Kumar S <74402250+Naveen-7u6@users.noreply.github.com>
2021-08-01 16:45:02 +0530

    String Reversal finished

---
+++ String Reversal.cpp
@@ -0,0 +1,16 @@
+/*Leetcode
+
+ Qs - https://leetcode.com/problems/reverse-string/description/
+
+
+*/
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        for(int i = 0, j = s.size() - 1; i < j; i++, j--){
+            swap(s[i], s[j]);
+            
+        }
+        
+    }
+};
