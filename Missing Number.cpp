b38e781c1dac7629de171391e65400f6f571670a
Naveen Kumar S <74402250+Naveen-7u6@users.noreply.github.com>
2021-08-03 13:56:21 +0530

    Update Missing Number.cpp

--- Missing Number.cpp
+++ Missing Number.cpp
@@ -4,3 +4,13 @@
 Qs - GG - https://practice.geeksforgeeks.org/problems/missing-number/0
 
 */
+int missingNumber(int a[], int n)
+{
+   
+     int n_elements_sum = n * (n + 1) / 2 ;
+  int sum = 0;
+ 
+  for(int i = 0; i < n - 1; i++)
+    sum += a[i];
+  return n_elements_sum-sum;
+}
